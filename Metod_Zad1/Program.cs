2d62eb2ba517febfab26383de9ecaafbdf3ab4aa
nvrsk <nvrsk@TUF-GAMINGF17>
2021-12-04 11:17:28 +0300

    Добавьте файлы проекта.

---
+++ Metod_Zad1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Metod_Zad1
+{
+    class Program
+    {
+        static void Perimetr(int a, int b, int c)
+        {
+            int Per = a + b + c;
+            Console.WriteLine($"Периметр тругла равен = {Per}");
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите стороны треугольника:");
+            Perimetr(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));           
+        
+        }
+
+    }
+}
